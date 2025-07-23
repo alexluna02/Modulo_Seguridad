d69fe57d824e72a8f1ff0a028e6ae184a995591d
alexluna02 <alexlunamay2002@gmail.com>
2025-07-23 00:06:23 -0500

    sin cors

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-
-app.use(cors({
-  origin: ['https://front-modulo-sp.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Si usas cookies o credenciales
-}));
-
-
+app.use(cors());
 app.use(express.json());
 
 // Verificar conexión a la base de datos antes de cada solicitud
