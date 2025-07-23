71b32c9c67da1693a000b17a07ba61ed3026932e
alexluna02 <alexlunamay2002@gmail.com>
2025-07-22 23:59:22 -0500

    cors para vercel

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://front-modulo-sp.vercel.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Si usas cookies o credenciales
+}));
+
+
 app.use(express.json());
 
 // Verificar conexión a la base de datos antes de cada solicitud
